52891fb4345b22be2434668fe9a9610c503d7ed8
OH-JOO-YEONG <brb1111@naver.com>
2023-06-08 23:38:26 +0900

    [Feat] 프로필 세팅 리스트 UI 1차
    - 다크모드, 언어, 로그아웃
    
    #1

--- singrum/components/profileScreen/SettingItem.js
+++ singrum/components/profileScreen/SettingItem.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
-const SettingItem = ({icon, name, isSub, subItem}) => {
+const SettingItem = ({icon, name, subItem}) => {
   return (
     <View style={styles.container}>
       {/* TODO - 아이콘 넣기 vector icon 작업 */}
-      {/* <View>{icon}</View> */}
-
-      <Text style={styles.name}>{name}</Text>
-
-      {/* TODO - sub item이 있으면 children 형태로 넣어주기 */}
-      {/* {isSub && <View>{subItem}</View>} */}
+      <View style={styles.iconWithName}>
+        {icon && <View style={styles.icon}>{icon}</View>}
+        <Text style={styles.name}>{name}</Text>
+      </View>
+      <View>
+        {/* TODO - sub item이 있으면 children 형태로 넣어주기 */}
+        {subItem && <View style={styles.subWrapper}>{subItem}</View>}
+      </View>
     </View>
   );
 };
@@ -19,11 +21,26 @@ const styles = StyleSheet.create({
   container: {
     width: '100%',
     paddingVertical: 16,
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+  },
+  iconWithName: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  icon: {
+    width: 24,
+    marginRight: 8,
+    alignItems: 'center',
   },
   name: {
     fontSize: 14,
     fontWeight: '600',
     color: '#000',
+  },
+  subWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
 });
 
--- singrum/components/profileScreen/SettingList.js
+++ singrum/components/profileScreen/SettingList.js
@@ -1,18 +1,105 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import SettingItem from './SettingItem';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import FontAwesomeIcons from 'react-native-vector-icons/FontAwesome';
+import {Switch} from 'react-native-switch';
 
 const SettingList = () => {
+  const [currentLanguage, setCurrentLanguage] = useState('kr');
+  const [currentTheme, setCurrentTheme] = useState('light');
+  const login = true;
+
+  const toggleLanguage = useCallback(() => {
+    if (currentLanguage === 'kr') {
+      setCurrentLanguage('en');
+    } else {
+      setCurrentLanguage('kr');
+    }
+  }, [currentLanguage]);
+
+  const toggleTheme = useCallback(() => {
+    if (currentTheme === 'light') {
+      setCurrentTheme('dark');
+    } else {
+      setCurrentTheme('light');
+    }
+  }, [currentTheme]);
+
+  let settingComponent;
+  if (login) {
+    settingComponent = (
+      <>
+        <SettingItem
+          icon={<MaterialIcons name="language" color={'#787276'} size={22} />}
+          name={'언어'}
+          subItem={
+            <Switch
+              value={currentLanguage === 'kr' ? true : false}
+              onValueChange={toggleLanguage}
+              circleSize={30}
+              barHeight={30}
+              circleBorderWidth={1}
+              activeText={''}
+              inActiveText={''}
+              changeValueImmediately={true}
+              switchLeftPx={2} // denominator for logic when sliding to TRUE position. Higher number = more space from RIGHT of the circle to END of the slider
+              switchRightPx={2} // denomina
+              renderInsideCircle={() => {
+                let insideText;
+                if (currentLanguage === 'kr') insideText = <Text>KR</Text>;
+                else insideText = <Text>EN</Text>;
+                return insideText;
+              }}
+            />
+          }
+        />
+        <SettingItem
+          icon={<FontAwesomeIcons name="moon-o" color={'#787276'} size={22} />}
+          name={'다크 모드'}
+          subItem={
+            <Switch
+              value={currentTheme === 'light' ? true : false}
+              onValueChange={toggleTheme}
+              circleSize={30}
+              barHeight={30}
+              circleBorderWidth={1}
+              activeText={''}
+              inActiveText={''}
+              changeValueImmediately={true}
+              switchLeftPx={2} // denominator for logic when sliding to TRUE position. Higher number = more space from RIGHT of the circle to END of the slider
+              switchRightPx={2} // denomina
+            />
+          }
+        />
+        <SettingItem
+          icon={<MaterialIcons name="logout" color={'#787276'} size={22} />}
+          name={'로그아웃'}
+        />
+      </>
+    );
+  } else {
+    <>
+      <SettingItem
+        icon={<MaterialIcons name="language" color={'#787276'} size={22} />}
+        name={'언어'}
+      />
+      <SettingItem
+        icon={<FontAwesomeIcons name="moon-o" color={'#787276'} size={22} />}
+        name={'다크 모드'}
+      />
+      <SettingItem
+        icon={<MaterialIcons name="logout" color={'#787276'} size={22} />}
+        name={'로그인/회원가입'}
+      />
+    </>;
+  }
   return (
     <View style={styles.container}>
       <View style={styles.settingTitleWrapper}>
-        <Text style={styles.settingTitle}>PREFERENCES</Text>
+        <Text style={styles.settingTitle}>설정</Text>
       </View>
-      <View style={styles.settingWrapper}>
-        <SettingItem name={'언어'} />
-        <SettingItem name={'다크 모드'} />
-        <SettingItem name={'로그아웃'} />
-      </View>
+      <View style={styles.settingWrapper}>{settingComponent}</View>
     </View>
   );
 };
