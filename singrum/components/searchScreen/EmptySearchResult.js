da08c65b030bb7272677c78b741ddd21b1735e7d
ming0830 <minsun0830@naver.com>
2023-06-29 16:41:12 +0900

    [Feat] 검색 결과 없을 경우, 안내 문구 컴포넌트

---
+++ singrum/components/searchScreen/EmptySearchResult.js
@@ -0,0 +1,26 @@
+const {View, Text, StyleSheet} = require('react-native');
+
+const messages = {
+  NOT_FOUND: '검색 결과가 없습니다.',
+};
+
+function EmptySearchResult({type}) {
+  return (
+    <View style={styles.block}>
+      <Text stlye={styles.text}>{messages[type]}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+    alignContent: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 16,
+  },
+});
+
+export default EmptySearchResult;
